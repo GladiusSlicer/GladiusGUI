3a3d0d0cb8cc8ac14e06d96ee8e8e772148d8de0
mrhatman <lucasince@gmail.com>
2022-03-11 19:21:13 -0600

    Update Shader

--- src/shaders.rs
+++ src/shaders.rs
@@ -10,7 +10,7 @@ pub const VERTEX_SHADER_SRC: &str = r#"
     uniform mat4 model;
     void main() {
         mat4 modelview = view * model;
-        viewPosition = (view * vec4(position, 1.0)).xyz;
+        viewPosition = (modelview * vec4(position, 0.0)).xyz;
         gl_Position = perspective * modelview * vec4(position, 1.0);
         v_color = color;
 
@@ -25,16 +25,17 @@ pub const FRAGMENT_SHADER_SRC: &str = r#"
         in vec3 v_color;
         out vec4 color;
         void main() {
-            vec3 xTangent = dFdx( viewPosition );
-            vec3 yTangent = dFdy( viewPosition );
+            vec3 xTangent = vec3(dFdx(viewPosition.x),dFdx(viewPosition.y),dFdx(viewPosition.z));
+            vec3 yTangent = vec3(dFdy(viewPosition.x),dFdy(viewPosition.y),dFdy(viewPosition.z));
             vec3 faceNormal = normalize( cross( xTangent, yTangent ));
+
 
             const vec3 ambient_color = vec3(0.0, 0.0, 0.0);
             vec3 diffuse_color = v_color;
             const vec3 specular_color = vec3(1.0, 1.0, 1.0);
 
             float diffuse = max(dot(normalize(faceNormal), normalize(vec3(0., 0.0, 0.1))), 0.0);
-            vec3 camera_dir = normalize(-v_position);
+            vec3 camera_dir = -normalize(v_position);
             vec3 half_direction = normalize(camera_dir);
             float specular = pow(max(dot(half_direction, -normalize(faceNormal)), 0.0), 128.0);
             color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
